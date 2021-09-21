fd72801349a055c56cf357df0230308d8389e8e4
Rajarshi Banerjee <rajarshiban13@gmail.com>
2021-09-22 02:15:57 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup,find_packages
+
+classifiers=[
+    'Intendent Audience :: Education',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 2',
+    'License :: OSI Approved :: MIT License'
+    
+]
+
+setup(name='pyDS',
+version='0.0.1',
+description='Some useful and known data structure.',
+url='#',
+author='Rajarshi Banerjee',
+author_email='sambaner1050@outlook.com ',
+license='MIT',
+keywords=['python','linked list', 'singly linked list'],
+classifiers=classifiers,
+packages=find_packages(),
+install_requires=[],
+
+zip_safe=False)
